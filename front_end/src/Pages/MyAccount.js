32695e673c862a71e4830a868a5f4e399af5d9eb
Simran Nijjar <simran.nijjar@hotmail.com>
2022-07-30 20:54:13 -0700

    MyAccount.js

--- front_end/src/Pages/MyAccount.js
+++ front_end/src/Pages/MyAccount.js
@@ -1,13 +1,30 @@
-import React, {Component} from 'react'; 
-
+import {React, Component} from 'react'; 
 
 export class MyAccount extends Component{
   render () {
+
     return(
-    <div>
-      <h3 className="d-flex justify-content-center m-3">
-        This is a My Account Page
-      </h3>
+      <div className="p-10 text-center bg-white">
+      <h1 className="mb-3 text-success">My Account Page</h1>
+      <h4 className="mb-3">Here you can update your phone number or delete your account</h4>
+      <table className="table table-success table-striped table-hover">
+        <thead>
+          <tr> 
+            <th> Delete Account</th>
+            <th> Update Phone Number</th>
+          </tr>
+          </thead>
+          <tbody>
+
+          {/* <td><h6>Click Delete Account to Delete</h6> */}
+          {/* <div className= "mb-3" > <a href="/DeleteAccount" className="btn btn-outline-success">Delete Account</a></div></td> */}
+
+
+          <td><h6>Click Update Phone Number to Update</h6>
+          <button className="btn btn-success mb-4">Update Phone Number</button></td>
+                  
+          </tbody>
+      </table>
     </div>
     )
   }
