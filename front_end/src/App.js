e1503ae1003ea44ad1cfc67c47033dd3f94ab2af
Simran Nijjar <simran.nijjar@hotmail.com>
2022-08-01 14:08:55 -0700

    Removed Search Bar

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -110,10 +110,6 @@ function App() {
 
 
             </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
